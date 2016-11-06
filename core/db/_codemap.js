61f33f7f0509a345bcb221a261b7d8230c224879
Patrick Frischmann <pgfrischmann@gmail.com>
2016-11-06 21:09:45 +0100

    * added mems DB for learner

--- core/db/_codemap.js
+++ core/db/_codemap.js
@@ -10,19 +10,22 @@ module.exports = {
   'db.run_states': require('./run_states'),
   'db.thoughts': require('./thoughts'),
   'db.ticks': require('./ticks'),
+  'db.mems': require('./mems'),
   'zenbrain:cache': '#motley:db.cache',
   'zenbrain:locks': '#motley:db.locks',
   'zenbrain:logs': '#motley:db.logs',
   'zenbrain:run_states': '#motley:db.run_states',
   'zenbrain:thoughts': '#motley:db.thoughts',
   'zenbrain:ticks': '#motley:db.ticks',
+  'zenbrain:mems': '#motley:db.mems',
   'db.collections[]': [
     '#db.cache',
     '#db.locks',
     '#db.logs',
     '#db.run_states',
     '#db.thoughts',
-    '#db.ticks'
+    '#db.ticks',
+    '#db.mems'
   ],
   'zenbrain:db_hooks': [],
   'conf.db.mongo{}': function container (get, set, clear) {
